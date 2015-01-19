6bbb8a905f793a4e2f0d481d7ce099c7b3c665e7
Duncan Cameron <phplist@dcameron.me.uk>
2015-01-19 07:25:43 +0000

    Correct line endings

--- plugins/SubscribersPlugin/help/en/details.php
+++ plugins/SubscribersPlugin/help/en/details.php
@@ -1,1 +1,70 @@
-<?php/** * SubscribersPlugin for phplist *  * This file is a part of SubscribersPlugin. * * SubscribersPlugin is free software: you can redistribute it and/or modify * it under the terms of the GNU General Public License as published by * the Free Software Foundation, either version 3 of the License, or * (at your option) any later version. * SubscribersPlugin is distributed in the hope that it will be useful, * but WITHOUT ANY WARRANTY; without even the implied warranty of * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the * GNU General Public License for more details. *  * @category  phplist * @package   SubscribersPlugin * @author    Duncan Cameron * @copyright 2011-2013 Duncan Cameron * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3 *//** * Help text *  * @category  phplist * @package   SubscribersPlugin */?><p>The page displays the status and attributes for each subscriber.</p><p>You can filter the results by <ul><li>searching on the email address or value of an attribute,<li>subscribers who belong to a specific list,<li>restricting to confirmed subscribers or unconfirmed subscribers,<li>restricting to blacklisted subscribers or not blacklisted subscribers.</ul></p><p>Attributes can be displayed as separate columns in the listing. The form shows only the first 15 attributes, with the attribute name truncated to 20 characters.</p><p>If no attributes have been created then the selection form is not displayed.</p><table><tr><td>Export</td><td>The fields displayed can be exported as a CSV file.</td></tr><tr><td>email</td><td>The email address of the subscriber, which is a link to the Subscriber Details page.</td></tr><tr><td>Confirmed</td><td>An icon is displayed if the email address is unconfirmed.</td></tr><tr><td>Blacklisted</td><td>An icon is displayed if the subscriber is blacklisted.</td></tr><tr><td>Attributes</td><td>Each attribute that has been selected is displayed in a separate column.</td></tr><tr><td>HTML</td><td>Whether the subscriber should be sent HTML format emails.</td></tr><tr><td>Lists</td><td>The number of lists to which the subscriber belongs.</td></tr></table>
+<?php
+/**
+ * SubscribersPlugin for phplist
+ * 
+ * This file is a part of SubscribersPlugin.
+ *
+ * SubscribersPlugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * SubscribersPlugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @category  phplist
+ * @package   SubscribersPlugin
+ * @author    Duncan Cameron
+ * @copyright 2011-2013 Duncan Cameron
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
+ */
+
+/**
+ * Help text
+ * 
+ * @category  phplist
+ * @package   SubscribersPlugin
+ */
+?>
+<p>The page displays the status and attributes for each subscriber.</p>
+<p>You can filter the results by 
+<ul>
+<li>searching on the email address or value of an attribute,
+<li>subscribers who belong to a specific list,
+<li>restricting to confirmed subscribers or unconfirmed subscribers,
+<li>restricting to blacklisted subscribers or not blacklisted subscribers.
+</ul>
+</p>
+<p>Attributes can be displayed as separate columns in the listing. The form shows only the first 15 attributes, with the attribute name truncated to 20 characters.</p>
+<p>If no attributes have been created then the selection form is not displayed.</p>
+<table>
+<tr>
+<td>Export</td>
+<td>The fields displayed can be exported as a CSV file.</td>
+</tr>
+<tr>
+<td>email</td>
+<td>The email address of the subscriber, which is a link to the Subscriber Details page.</td>
+</tr>
+<tr>
+<td>Confirmed</td>
+<td>An icon is displayed if the email address is unconfirmed.</td>
+</tr>
+<tr>
+<td>Blacklisted</td>
+<td>An icon is displayed if the subscriber is blacklisted.</td>
+</tr>
+<tr>
+<td>Attributes</td>
+<td>Each attribute that has been selected is displayed in a separate column.</td>
+</tr>
+<tr>
+<td>HTML</td>
+<td>Whether the subscriber should be sent HTML format emails.</td>
+</tr>
+<tr>
+<td>Lists</td>
+<td>The number of lists to which the subscriber belongs.</td>
+</tr>
+</table>
--- plugins/SubscribersPlugin/help/en/subscriptions.php
+++ plugins/SubscribersPlugin/help/en/subscriptions.php
@@ -1,1 +1,41 @@
-<p>This page displays a summary by month of subscriptions and unsubscriptions.</p><p>The chart shows subscriptions by month for the periods in the result table. Each bar has up to threesections: active, blacklisted and unconfirmed. Together these add-up to the number of subscriptions.</p><p>A line showing the number of unsubscriptions per month is overlaid on the bar chart.</p><table><tr><td>Period</td><td>The year and month.</td></tr><tr><td>Subscriptions</td><td>The number of new subscribers during the month. This total is calculated using the 'entered' date in the user table.The value will be the sum of the next three columns: active, blacklisted and unconfirmed.</td></tr><tr><td>Active</td><td>The number of subscribers who are now confirmed and not blacklisted.</td></tr><tr><td>Blacklisted</td><td>The number of subscribers who are now confirmed and blacklisted. The subscribers did not necessarily change to blacklisted in this month.</td></tr><tr><td>Unconfirmed</td><td>The number of subscribers who are unconfirmed.</td></tr><tr><td>Unsubscriptions</td><td>The number of unsubscriptions. This value is different to the Blacklisted total as it reflects when the unsubscription occured rather than when the subscription occured.</td></tr><tr><td>Total</td><td>Sum of the values for each month displayed for each column.</td></tr></table>
+<p>
+This page displays a summary by month of subscriptions and unsubscriptions.
+</p>
+<p>The chart shows subscriptions by month for the periods in the result table. Each bar has up to three
+sections: active, blacklisted and unconfirmed. Together these add-up to the number of subscriptions.</p>
+<p>A line showing the number of unsubscriptions per month is overlaid on the bar chart.
+</p>
+<table>
+<tr>
+<td>Period</td>
+<td>The year and month.</td>
+</tr>
+<tr>
+<td>Subscriptions</td>
+<td>The number of new subscribers during the month. This total is calculated using the 'entered' date in the user table.
+The value will be the sum of the next three columns: active, blacklisted and unconfirmed.</td>
+</tr>
+<tr>
+<td>Active</td>
+<td>The number of subscribers who are now confirmed and not blacklisted.</td>
+</tr>
+<tr>
+<td>Blacklisted</td>
+<td>The number of subscribers who are now confirmed and blacklisted. 
+The subscribers did not necessarily change to blacklisted in this month.</td>
+</tr>
+<tr>
+<td>Unconfirmed</td>
+<td>The number of subscribers who are unconfirmed.</td>
+</tr>
+<tr>
+<td>Unsubscriptions</td>
+<td>The number of unsubscriptions. 
+This value is different to the Blacklisted total as it reflects when the unsubscription occured rather than when the subscription occured.
+</td>
+</tr>
+<tr>
+<td>Total</td>
+<td>Sum of the values for each month displayed for each column.</td>
+</tr>
+</table>
